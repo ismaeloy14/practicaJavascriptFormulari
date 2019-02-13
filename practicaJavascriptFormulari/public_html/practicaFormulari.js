4c9eadbdb8c8f394fe79f8bb85634beaded3f14f
Ismael Angulo <ismaeloy14@gmail.com>
2019-02-13 19:18:41 +0100

    Ismael: ya sale los formularios

--- practicaJavascriptFormulari/public_html/practicaFormulari.js
+++ practicaJavascriptFormulari/public_html/practicaFormulari.js
@@ -6,12 +6,28 @@
 
 
 $("#botonNuevoFormulario").click(function(){
-    var nombreFormulario = $("#inputNomFormulari");
+  
+    //alert($("#inputNomFormulari").val());
+    var nombreFormulario = $("#inputNomFormulari").val();
+    var textoNomForm = document.createTextNode(nombreFormulario); 
     var newDiv = document.createElement("div");
     newDiv.setAttribute('class','divs');
+    
     var currentDiv = document.getElementById("divFormularis");
-    document.body.insertBefore(newDiv, currentDiv);
+    //document.body.insertBefore(newDiv, currentDiv);
+    currentDiv.appendChild(newDiv);
+    
+    /*var contendo = document.createTextNode("sddsfdsfdsfdsfdsffdsfdsdsfdsf");
+    newDiv.appendChild(contendo);*/
     
     var newForm = document.createElement("form");
-    document.body.insertBefore(newForm, newDiv);
+    //document.body.insertBefore(newForm, newDiv);
+    
+    newForm.method = 'POST';
+    var newLabel = document.createElement("label");
+    //document.body.insertBefore(newLabel, newForm);
+    newLabel.appendChild(textoNomForm);
+    newForm.appendChild(newLabel);
+    
+    newDiv.appendChild(newForm);
 });
